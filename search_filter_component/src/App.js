5e4cca998c04a1a9d1e3cc6d8a6cafea76db43d6
sstabile <stevenstabile@gmail.com>
2025-03-04 20:30:24 -0800

    Added routes and 250, 450 rider lists

--- search_filter_component/src/App.js
+++ search_filter_component/src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import { FilteredSearch } from './FilteredSearch';
 import './App.css';
+
+const twoFiftyRiders = [
+  "Haiden Deegan",
+  "Tom Vialle",
+  "Levi Kitchen",
+  "Michael Moseman",
+  "Max Anstie"
+];
+
+const fourFiftyRiders = [
+  "Malcolm Stewart",
+  "Eli Tomac",
+  "Ken Roczen",
+  "Cooper Webb",
+  "Chase Sexton"
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 style={{fontSize:"30px"}}>Rider Search:</h1>
-        <FilteredSearch />
+        <Routes>
+          <Route path="/filter/250" element={<FilteredSearch riderList={twoFiftyRiders} />} />
+          <Route path="/filter/450" element={<FilteredSearch riderList={fourFiftyRiders} />} />
+          <Route path="/filter/all" element={<FilteredSearch riderList={[...twoFiftyRiders, ...fourFiftyRiders]} />} />
+        </Routes>
       </header>
     </div>
   );
--- search_filter_component/src/FilteredSearch.jsx
+++ search_filter_component/src/FilteredSearch.jsx
@@ -1,25 +1,29 @@
 import { useState } from "react";
-export const FilteredSearch = () =>
+export const FilteredSearch = ({riderList}) =>
 {
-    const riders = [
-        "Levi Kitchen",
-        "Haiden Deegan",
-        "Michael Moseman"
-    ];
+    // const riders = [
+    //     "Levi Kitchen",
+    //     "Haiden Deegan",
+    //     "Michael Moseman"
+    // ];
 
+    const [riderData, setRiderData] = useState(riderList);
     const [inputValue, setInputValue] = useState("");
 
-    const onInputChange = (e) => 
+    const handleInputChange = (e) => 
     {
         setInputValue(e.target.value);
     }
 
+    const riderDataFiltered = riderData.filter((rider) => 
+        rider.toLowerCase().includes(inputValue.toLowerCase())
+    );
+
     return (
         <div style={{textAlign:"left"}}>
-            <input type="text" placeholder="Search for a rider..." onChange={onInputChange} />
-            <p>{inputValue}</p>
-            <ul style={{listStyle:"none"}}>
-                {riders.map((rider) => {
+            <input type="text" placeholder="Search for a rider..." onChange={handleInputChange} />
+            <ul>
+                {riderDataFiltered.map((rider) => {
                     return <li>{rider}</li>
                 })}
             </ul>
